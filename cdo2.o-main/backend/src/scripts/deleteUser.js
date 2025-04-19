7afbd7f3f29bb7ad333b1c3e14b86b8b57751d66
Anubhav Yadav <yadavanubhav848@gmail.com>
2025-04-19 13:28:16 +0530

    Update deleteUser.js

--- cdo2.o-main/backend/src/scripts/deleteUser.js
+++ cdo2.o-main/backend/src/scripts/deleteUser.js
@@ -8,7 +8,7 @@ dotenv.config();
 async function deleteUser() {
   try {
     // Connect to MongoDB
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/billing-portal');
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://yadavanubhav848:alm15lfJPPtmob4k@cluster0.szpod3z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('Connected to MongoDB');
 
     // Delete user
